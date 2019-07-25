abf467779414bc3c506496a30d480e72568c5edc
iilness2 <andre.aliaman90@gmail.com>
2019-07-25 20:57:22 +0700

    update the custom and save

--- publish-staging.sh
+++ publish-staging.sh
@@ -2,14 +2,14 @@
 
 # AWS Regions
 REGIONS=(
-    "us-west-2"
+    "ap-southeast-2"
 )
 LAYER_NAME="bash-testing"
 
 for region in ${REGIONS[@]}; do
     echo "Publishing layer to $region..."
 
-    LAYER_ARN=$(aws lambda publish-layer-version --region $region --layer-name $LAYER_NAME --description "Bash in AWS Lambda [https://github.com/gkrizek/bash-lambda-layer]" --compatible-runtimes provided --license MIT --zip-file fileb://export/layer.zip | jq -r .LayerVersionArn)
+    LAYER_ARN=$(aws lambda publish-layer-version --region $region --layer-name $LAYER_NAME --description "Bash in AWS Lambda [https://github.com/iilness2/bash-lambda-layer-custom.git]" --compatible-runtimes provided --license MIT --zip-file fileb://export/layer.zip | jq -r .LayerVersionArn)
     
     echo $LAYER_ARN
     echo "$region complete for Staging"
